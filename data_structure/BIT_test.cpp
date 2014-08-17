2d6cfa8fe073c6505993bd7b6de584b9c1a22c53
ichyo <syo1990@gmail.com>
2014-08-17 13:24:01 +0900

    Add test for BIT

--- data_structure/BIT_test.cpp
+++ data_structure/BIT_test.cpp
@@ -1,5 +1,20 @@
 #include "gtest/gtest.h"
+#include "./BIT.cpp"
 
-TEST(FactorialTest, HandlesZeroInput) {
-      EXPECT_EQ(1, 1);
+TEST(BIT, SequentialTest){
+    int n = 1000000;
+    BIT bit(n);
+    for(int i = 0; i < n; i++) {
+        bit.add(i, 1);
+        ASSERT_EQ(bit.get(i), i + 1);
+    }
+}
+
+TEST(BIT, SequentialTestLL){
+    int n = 1000000;
+    BIT bit(n);
+    for(int i = 0; i < n; i++) {
+        bit.add(i, 1000000);
+        ASSERT_EQ(bit.get(i), 1000000LL * (i + 1));
+    }
 }
