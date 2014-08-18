a8261fc14e5d27434905a55ea321ca7235e18395
ichyo <syo1990@gmail.com>
2014-08-18 22:06:40 +0900

    Add BITRange

--- data_structure/BIT.cpp
+++ data_structure/BIT.cpp
@@ -5,7 +5,7 @@
 // Binary Indexed Tree (Fenwick Tree) (0-indexed)
 // two queries in O(log n)
 // 1. add w to v[at]
-// 2. the sum of v_0, v_1, .., v_at
+// 2. the sum of v[0], v[1], .., v[at]
 struct BIT{
     vector<LL> sums;
     BIT(int n) : sums(n) {}
@@ -31,7 +31,27 @@ struct BIT{
 
 // BIT (range-version) (0-indexed)
 // two queries in O(log n)
-// 1. add w to v_a, v_{a + 1}, ..., v_b
-// 2. get the sum of v_0, v_1, ..., v_{c - 1}
+// 1. add w to v[a], v[a+1], ..., v[b-1]
+// 2. get the sum of v[0], v[1], ..., v[c-1]
+//
+struct BITRange{
+    BIT bit0, bit1;
+    BITRange(int n) : bit0(n + 1), bit1(n + 1) {}
+
+    // v[a], v[a+1], ..., v[b-1] += by
+    void add(int a, int b, LL by) {
+        bit0.add(a, -by * a);
+        bit0.add(b, +by * b);
+        bit1.add(a, by);
+        bit1.add(b, -by);
+    }
+
+    // v[0] + v[1] + ... + v[c-1]
+    LL get(int c) {
+        LL A = bit0.get(c);
+        LL B = bit1.get(c);
+        return A + B * c;
+    }
+};
 
 
--- data_structure/BIT_test.cpp
+++ data_structure/BIT_test.cpp
@@ -18,3 +18,28 @@ TEST(BIT, SequentialTestLL){
         ASSERT_EQ(bit.get(i), 1000000LL * (i + 1));
     }
 }
+
+TEST(BITRange, Random) {
+    int n = 10000;
+    vector<LL> sum(n);
+    BITRange bit(n);
+    int x, y, w;
+    for(int i = 0; i < n; i++) {
+        x = rand() % (n - 1) + 1;
+        y = rand() % x;
+        w = rand();
+        swap(x, y);
+        for(int j = x; j <= y; j++) {
+            sum[j] += w;
+        }
+        bit.add(x, y + 1, w);
+
+        x = rand() % n;
+        LL res = 0;
+        for(int j = 0; j <= x; j++) {
+            res += sum[j];
+        }
+
+        ASSERT_EQ(bit.get(x + 1), res);
+    }
+}
