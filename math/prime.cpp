7a2b1e93942194d1054549061f904d66392a5166
ichyo <syo1990@gmail.com>
2015-03-23 02:21:00 +0900

    Refactor

--- math/prime.cpp
+++ math/prime.cpp
@@ -28,17 +28,19 @@ bool miller_rabin(LL n){
     return true;
 }
 
-// [a, b)の整数に対して素数テーブルを作る．is_prime[i - a] = true <-> iが素数
+// [a, b)の整数に対して素数テーブルを作る．is_prime[i-a]==true <-> iが素数
 vector<bool> segment_sieve(LL a, LL b){
+    assert(a < b);
+    assert(a >= 2);
     int q = int(sqrt(b) + 2);
     vector<bool> is_prime(b - a, true);
     vector<bool> is_prime_small(q, true);
 
-    for(int i = 2; (LL)i * i < b; i++){
+    for(int i = 2; (LL)i*i < b; i++){
         if(is_prime_small[i]){
-            for(int j = 2 * i; (LL)j * j < b; j += i)
+            for(int j = 2*i; (LL)j*j < b; j+=i)
                 is_prime_small[j] = false;
-            for(LL j = max(2LL, (a + i - 1) / i) * i; j < b; j += i)
+            for(LL j = max(2LL, (a+i-1)/i)*i; j < b; j+=i)
                 is_prime[j - a] = false;
         }
     }
