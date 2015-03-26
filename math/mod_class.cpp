78e6a69334ec241b5e210be5e6bbe893e2e885f6
ichyo <syo1990@gmail.com>
2015-03-26 14:49:19 +0900

    Add power function to ModInt

--- math/mod_class.cpp
+++ math/mod_class.cpp
@@ -85,5 +85,15 @@ template<int MOD>
 ostream& operator << (ostream& os, const ModInt<MOD> m) {
     return os << m.val;
 }
+template<int MOD, typename T>
+ModInt<MOD> pow(ModInt<MOD> a, T b) {
+    if(b == 0) {
+        return 1;
+    } else {
+        auto w = pow(a*a, b/2);
+        if(b&1) w *= a;
+        return w;
+    }
+}
 // }}}
 typedef ModInt<1000000007> mint;
